7b7326706d9f90dc247f61e2eafa2c8224fd3c47
mirii-ai <56605032+mirii-ai@users.noreply.github.com>
2021-11-09 12:05:08 +0000

    Changes. Added a change to the WordController file as I had put "GetMapping" when it should have been "PostMapping".

--- LanguageApp/src/main/java/com/qa/languages/rest/WordController.java
+++ LanguageApp/src/main/java/com/qa/languages/rest/WordController.java
@@ -3,9 +3,9 @@ package com.qa.languages.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,10 +25,10 @@ public class WordController {
 		this.service = service;
 	}
 
-	@GetMapping("/add")
-	public WordConstruct addNewWord(@RequestBody WordConstruct newWord) {
-		WordConstruct newItem = this.addNewWord(newWord);
-		return newItem;
+	@PostMapping("/add")
+	public ResponseEntity<WordConstruct> addNewWord(@RequestBody WordConstruct newWord) {
+		WordConstruct newItem = this.service.addNewWord(newWord);
+		return new ResponseEntity<WordConstruct>(newItem, HttpStatus.CREATED);
 	}
 
 	@PatchMapping("/memorised/{word_id}") // patch mapping- updates not puts. you don't change the data, updating things
